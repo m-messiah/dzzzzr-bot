ed8bdef5f66a1bd863559a0f189212f82db184b8
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-09-16 12:05:46 +0300

    sessions and resolving

--- main.py
+++ main.py
@@ -1,74 +1,172 @@
+# coding=utf-8
+from base64 import b64decode, b64encode
 from os import environ
 import urllib
-from flask import Flask, request, jsonify, json
+from zlib import decompress, MAX_WBITS
+from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify
 from google.appengine.api import urlfetch
 from requests import Session
+from re import compile as re_compile
+
 app = Flask(__name__)
-app.config['DEBUG'] = False
-app.browser = Session()
-app.dzzzr_url = ""
-app.code_prefix = ""
-
-
-from commands import CMD
+app.config['DEBUG'] = True
+dr_code = re_compile(ur"^[0-9]*[dDдД]?[0-9]*[rRрР][0-9]*$")
 
 try:
-    from bot_token import BOT_TOKEN, DR_LOGIN, DR_PASSWORD
+    from bot_token import BOT_TOKEN
 except ImportError:
     BOT_TOKEN = environ["TOKEN"]
-    DR_LOGIN = environ["DR_LOGIN"]
-    DR_PASSWORD = environ["DR_PASSWORD"]
+
+
+SESSIONS = {}
 
 URL = "https://api.telegram.org/bot%s/" % BOT_TOKEN
 MyURL = "https://dzzzr-bot.appspot.com"
 
 
-def set_dzzzr(arguments, chat_id):
-    try:
-        arguments = arguments.split()
-        if len(arguments) > 2:
-            url, captain, password = arguments[:3]
+class DozoR(object):
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+        self.url = ""
+        self.prefix = ""
+        self.browser = Session()
+
+    def set_dzzzr(self, arguments):
+        try:
+            arguments = arguments.split()
+            if len(arguments) > 2:
+                self.url, captain, pin, login, password = arguments[:5]
+            else:
+                raise ValueError
+
+            self.prefix = arguments[6] if len(arguments) > 5 else ""
+
+        except ValueError:
+            return {
+                'chat_id': self.chat_id,
+                'text': "Usage: \n"
+                        "/set_dzzzr url captain pin login password [prefix]"
+            }
         else:
-            raise ValueError
+            self.browser.headers.update({'referer': self.url})
+            self.browser.auth = (captain, pin)
+            login_page = self.browser.post(
+                self.url,
+                data={'login': login,
+                      'password': password,
+                      'action': "auth", 'notags': ''})
+            if login_page.status_code != 200:
+                return {
+                    'chat_id': self.chat_id,
+                    'text': "Not authorized"
+                }
+            else:
+                return {
+                    'chat_id': self.chat_id,
+                    'text': "Welcome %s" % login
+                }
 
-        prefix = arguments[3] if len(arguments) > 3 else ""
-
-    except ValueError:
+    def not_found(self, _):
         return {
-            'chat_id': chat_id,
-            'text': "Usage: /set_dzzzr url captain password [prefix]"
+            'chat_id': self.chat_id,
+            'text': "Command not found. Try /help"
         }
-    else:
-        app.dzzzr_url = url
-        app.code_prefix = prefix
-        app.browser.headers.update({'referer': app.dzzzr_url})
-        app.browser.auth = (captain, password)
-        login_page = app.browser.post(
-            app.dzzzr_url,
-            data={'login': DR_LOGIN,
-                  'password': DR_PASSWORD,
-                  'action': "auth", 'notags': ''})
-        if login_page.status_code != 200:
-            return {
-                'chat_id': chat_id,
-                'text': "Not authorized"
-            }
+
+    def start(self, _):
+        return {'chat_id': self.chat_id, 'text': "I am awake!"}
+
+    def about(self, _):
+        return {
+            'chat_id': self.chat_id,
+            'text': "Hey!\n"
+                    "My author is @m_messiah."
+                    "You can find this nickname at:"
+                    "\t+ Telegram"
+                    "\t+ Twitter"
+                    "\t+ Instagram"
+                    "\t+ VK"
+                    "\t+ GitHub (m-messiah)"
+        }
+
+    def base64(self, arguments):
+        response = {'chat_id': self.chat_id}
+        try:
+            response['text'] = b64decode(arguments.encode("utf8"))
+            assert len(response['text'])
+        except:
+            response['text'] = b64encode(arguments.encode("utf8"))
+        finally:
+            return response
+
+    def handle(self, text, reply_id=None):
+        if text[0] == '/':
+            command, _, arguments = text.partition(" ")
+            app.logger.debug("REQUEST\t%s\t%s\t'%s'",
+                             self.chat_id,
+                             command.encode("utf8"),
+                             arguments.encode("utf8"))
+            if command == "/set_dzzzr":
+                #if str(sender['id']) == "3798371":
+                try:
+                    return self.set_dzzzr(arguments)
+                except Exception as e:
+                    return {'chat_id': self.chat_id,
+                            'text': "Incorrect format (%s)" % e}
+            else:
+                return getattr(self, command[1:], self.not_found)(arguments)
         else:
-            return {
-                'chat_id': chat_id,
-                'text': "Welcome %s" % DR_LOGIN
-            }
+            response = self.code(text, reply_id)
+            if response:
+                return response
+
+    def code(self, text, reply_id):
+        def send(browser, url, code):
+            if url == "":
+                return code + u" - сначала надо войти в движок"
+            answer = browser.post(url, data={'action': "entcod",
+                                             'cod': code})
+            if not answer:
+                return code + u" - Нет ответа. Проверьте вручную."
+            answer = BeautifulSoup(
+                decompress(answer.content, 16 + MAX_WBITS)
+                .decode("cp1251", "ignore"),
+                'html.parser'
+            )
+
+            message = answer.find(class_="sysmsg")
+            return code + " - " + (message.string if message and message.string
+                                   else u"нет ответа.")
+
+        response = {'chat_id': self.chat_id}
+        if reply_id:
+            response['reply_to_message_id'] = reply_id
+
+        codes = text.split()
+        result = []
+        for code in codes:
+            if dr_code.match(code):
+                code = code.upper().translate({ord(u'Д'): u'D',
+                                               ord(u'Р'): u'R'})
+                if "D" not in code:
+                    code = self.prefix + code
+                result.append(send(self.browser, self.url, code))
+        if len(result):
+            response['text'] = u"\n".join(result).encode("utf8")
+            return response
+        else:
+            if u" бот" in text[-5:]:
+                response['text'] = u"хуебот".encode("utf8")
+                return response
+
+            if u"Привет" in text:
+                response['text'] = u"Привет!".encode("utf8")
+                return response
+            return None
 
 
 def error():
     return 'Hello World! I am DR bot (https://telegram.me/DzzzzR_bot)'
-
-
-def not_found(_, message):
-    return {
-        'chat_id': message['chat']['id'],
-        'text': "Command not found. Try /help"
-    }
 
 
 def send_reply(response):
@@ -98,7 +196,6 @@ def index():
             return error()
         app.logger.debug("Request: %s", request)
         try:
-        
             update = request.json
             message = update['message']
             sender = message['chat']
@@ -107,35 +204,12 @@ def index():
                 app.logger.debug("MESSAGE FROM\t%s",
                                  sender['username'] if 'username' in sender
                                  else sender['id'])
+                if sender['id'] not in SESSIONS:
+                    SESSIONS[sender['id']] = DozoR(sender['id'])
 
-                if text[0] == '/':
-                    command, _, arguments = text.partition(" ")
-                    app.logger.debug("REQUEST\t%s\t%s\t'%s'",
-                                     sender['id'],
-                                     command.encode("utf8"),
-                                     arguments.encode("utf8"))
-                    if command == "/set_dzzzr":
-                        if str(sender['id']) == "3798371":
-                            try:
-                                response = set_dzzzr(arguments, sender['id'])
-                            except Exception as e:
-                                response = {'chat_id': sender['id'],
-                                            'text': "Incorrect format (%s)" % e}
-                        else:
-                            response = {'chat_id': sender['id'],
-                                        'text': "Where is my master?"}
-                    else:
-                        response = CMD.get(command, not_found)(arguments,
-                                                               message)
-
+                response = SESSIONS[sender['id']].handle(text)
+                if response:
                     send_reply(response)
-                else:
-                    response = CMD["#code"](None, message,
-                                            app.browser,
-                                            app.dzzzr_url,
-                                            app.code_prefix)
-                    if response:
-                        send_reply(response)
 
             return jsonify(result="OK", text="Accepted")
         except Exception as e:
