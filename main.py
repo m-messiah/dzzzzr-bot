f906380c6427a4b3b7853eb34d182952b2efc253
Maxim Muzafarov <m.muzafarov@gmail.com>
2016-06-30 11:52:22 +0000

    Add random User-agents (one for session) and some PEP8 (not tested)

--- main.py
+++ main.py
@@ -17,6 +17,195 @@ __author__ = 'm_messiah'
 SESSIONS = {}
 CREDENTIALS = {}
 
+USERAGENTS = (
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) '
+    'Chrome/25.0.1364.172 YaBrowser/1.7.1364.22194 Safari/537.22',
+    '(X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
+    '(Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 '
+    'Firefox/3.5.3 (.NET CLR 3.5.30729)',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 '
+    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
+    '(iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
+    '(iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
+    '(iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
+    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+    '(Linux; Android 4.1.2; GT-I9100 Build/JZO54K) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.82 Mobile Safari/537.36',
+    '(Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 '
+    'Firefox/3.5.3 (.NET CLR 3.5.30729)',
+    '(Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 '
+    'Firefox/3.5.1',
+    '(Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 '
+    '(KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1',
+    '(compatible; MSIE 8.0; Windows NT 6.1; WOW64; '
+    'Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)',
+    '(compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; '
+    '.NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; '
+    '.NET CLR 3.0.30729)',
+    '(compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)',
+    '(compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    '(compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
+    '(compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; '
+    '.NET CLR 2.0.50727; InfoPath.2)',
+    '(Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+    '(compatible; MSIE 7.0; Windows NT 5.1)',
+    '(compatible; MSIE 6.1; Windows XP)',
+    '(Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 '
+    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    '(Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Windows NT 5.1; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
+    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
+    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Windows NT 6.2; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(iPad; CPU OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
+    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.30.1 '
+    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+    '(compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
+    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 '
+    '(KHTML, like Gecko) Version/7.0 Safari/537.71',
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
+    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 '
+    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
+    '(X11; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    '(compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    '(Macintosh; Intel Mac OS X 10.7; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.59.10 '
+    '(KHTML, like Gecko) Version/5.1.9 Safari/534.59.10',
+    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    '(compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.66 Safari/537.36',
+    '(iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
+    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10.6; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0',
+    '(Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0',
+    '(iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
+    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
+    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    '(iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
+    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
+    '(Windows NT 6.1; rv:23.0) Gecko/20100101 Firefox/23.0',
+    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    '(Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.16',
+    '(iPad; CPU OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
+    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
+    '(Windows NT 6.2; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
+    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+    '(Linux; Android 4.1.2; GT-I9100 Build/JZO54K) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.82 Mobile Safari/537.36',
+    '(Windows NT 6.0; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.65 Safari/537.36',
+    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.101 Safari/537.36',
+    '(Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.69 Safari/537.36',
+    '(compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    '(Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0',
+    '(iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 '
+    '(KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3',
+    '(Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0',
+    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/29.0.1547.76 Safari/537.36',
+    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
+    '(X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) '
+    'Chrome/26.0.1410.63 Safari/537.31',
+    '(Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+    '(Windows NT 6.3; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
+    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/30.0.1599.66 Safari/537.36',
+)
+
 RUS = (1072, 1073, 1074, 1075, 1076, 1077, 1105, 1078, 1079, 1080, 1081, 1082,
        1083, 1084, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094,
        1095, 1096, 1097, 1098, 1099, 1100, 1101, 1102, 1103)
@@ -81,12 +270,14 @@ class DozoR(object):
                 u"/set_dzzzr url captain pin login password [prefix] [regexp]")
         else:
             merged = "|".join((self.url, captain, login))
-            if (merged in CREDENTIALS and
-                        self.chat_id != CREDENTIALS[merged]):
+            if (merged in CREDENTIALS and self.chat_id != CREDENTIALS[merged]):
                 return (u"Бот уже используется этой командой. В чате %s\n"
                         u"Сначала остановите его. (/stop)\n"
                         % SESSIONS[CREDENTIALS[merged]].title)
-            self.browser.headers.update({'referer': self.url})
+            self.browser.headers.update({
+                'referer': self.url,
+                'User-Agent': "Mozilla/5.0 " + choice(USERAGENTS)
+            })
             self.browser.auth = (captain, pin)
             login_page = self.browser.post(
                 self.url,
@@ -132,7 +323,7 @@ class DozoR(object):
             chat_id, _, text = arguments.partition(" ")
             payload = urlencode({'chat_id': chat_id,
                                  'text': text.encode("utf8")})
-            o = urlfetch.fetch(
+            urlfetch.fetch(
                 "https://api.telegram.org/"
                 "bot117677541:AAEKw2NOQabXRGwr-Edvqx1RJ6Z73Xjxn8k/sendMessage",
                 payload=payload,
@@ -149,7 +340,7 @@ class DozoR(object):
                 continue
             payload = urlencode({'chat_id': chat_id,
                                  'text': text.encode("utf8")})
-            o = urlfetch.fetch(
+            urlfetch.fetch(
                 "https://api.telegram.org/"
                 "bot117677541:AAEKw2NOQabXRGwr-Edvqx1RJ6Z73Xjxn8k/sendMessage",
                 payload=payload,
@@ -229,7 +420,7 @@ class DozoR(object):
         try:
             response = b64decode(arguments.encode("utf8"))
             assert len(response)
-            _ = response.decode("utf8").encode("utf8")
+            response.decode("utf8").encode("utf8")
         except:
             response = b64encode(arguments.encode("utf8"))
         finally:
@@ -271,9 +462,10 @@ class DozoR(object):
                 result = []
                 for lat in coords[1]:
                     d, m, s = map(float, lat[:3])
-                    result.append(
-                        round(d + (m * 60 + s) / 3600 * (-1 if d < 0 else 1), 6)
-                    )
+                    result.append(round(
+                        d + (m * 60 + s) / 3600 * (-1 if d < 0 else 1),
+                        6
+                    ))
                 return tuple(result)
             return None
         except:
