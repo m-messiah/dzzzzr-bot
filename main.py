404e4bf442b6604e88af81cf359e5dd62f1ef140
Maxim Muzafarov <m-messiah@yandex-team.ru>
2017-05-31 22:43:58 +0300

    Move useragents to seaparate file

--- main.py
+++ main.py
@@ -12,199 +12,12 @@ from requests import Session
 from google.appengine.api import urlfetch
 from webapp2_extras import json
 
+from useragents import USERAGENTS
+
 __author__ = 'm_messiah'
 
 SESSIONS = {}
 CREDENTIALS = {}
-
-USERAGENTS = (
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) '
-    'Chrome/25.0.1364.172 YaBrowser/1.7.1364.22194 Safari/537.22',
-    '(X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
-    '(Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 '
-    'Firefox/3.5.3 (.NET CLR 3.5.30729)',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 '
-    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-    '(iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
-    '(iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
-    '(iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
-    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-    '(Linux; Android 4.1.2; GT-I9100 Build/JZO54K) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.82 Mobile Safari/537.36',
-    '(Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 '
-    'Firefox/3.5.3 (.NET CLR 3.5.30729)',
-    '(Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 '
-    'Firefox/3.5.1',
-    '(Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 '
-    '(KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1',
-    '(compatible; MSIE 8.0; Windows NT 6.1; WOW64; '
-    'Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)',
-    '(compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; '
-    '.NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; '
-    '.NET CLR 3.0.30729)',
-    '(compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)',
-    '(compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
-    '(compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
-    '(compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; '
-    '.NET CLR 2.0.50727; InfoPath.2)',
-    '(Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
-    '(compatible; MSIE 7.0; Windows NT 5.1)',
-    '(compatible; MSIE 6.1; Windows XP)',
-    '(Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 '
-    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
-    '(Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Windows NT 5.1; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
-    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
-    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Windows NT 6.2; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(iPad; CPU OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53',
-    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.30.1 '
-    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-    '(compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
-    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 '
-    '(KHTML, like Gecko) Version/7.0 Safari/537.71',
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
-    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 '
-    '(KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-    '(X11; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-    '(compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
-    '(Macintosh; Intel Mac OS X 10.7; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.59.10 '
-    '(KHTML, like Gecko) Version/5.1.9 Safari/534.59.10',
-    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
-    '(compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
-    '(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.66 Safari/537.36',
-    '(iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
-    '(Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10.6; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0',
-    '(Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0',
-    '(iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 '
-    '(KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
-    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-    '(iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
-    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
-    '(Windows NT 6.1; rv:23.0) Gecko/20100101 Firefox/23.0',
-    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-    '(Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.16',
-    '(iPad; CPU OS 6_1_3 like Mac OS X) AppleWebKit/536.26 '
-    '(KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25',
-    '(Windows NT 6.2; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
-    '(Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-    '(Linux; Android 4.1.2; GT-I9100 Build/JZO54K) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.82 Mobile Safari/537.36',
-    '(Windows NT 6.0; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.65 Safari/537.36',
-    '(X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.101 Safari/537.36',
-    '(Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.69 Safari/537.36',
-    '(compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
-    '(Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-    '(Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0',
-    '(iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 '
-    '(KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3',
-    '(Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0',
-    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/29.0.1547.76 Safari/537.36',
-    '(Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36',
-    '(X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) '
-    'Chrome/26.0.1410.63 Safari/537.31',
-    '(Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
-    '(Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-    '(Windows NT 6.3; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-    '(X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/30.0.1599.66 Safari/537.36',
-)
 
 RUS = (1072, 1073, 1074, 1075, 1076, 1077, 1105, 1078, 1079, 1080, 1081, 1082,
        1083, 1084, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094,
@@ -217,26 +30,6 @@ LITE_MESSAGE = re_compile(
     ur'<!--errorText--><p><strong>(.*?)</strong></p><!--errorTextEnd-->')
 LITE_TIME_ON = re_compile(ur'<!--timeOnLevelBegin (\d*?) timeOnLevelEnd-->')
 LITE_TIME_TO = re_compile(ur'<!--timeToFinishBegin (\d*?) timeToFinishEnd-->')
-
-
-def botan_track(chat_id, event, message):
-    try:
-        botan_url = "https://api.botan.io/track?"
-        botan_url += "token=ydRXN:ZK8C:Iq7BXBi9MUb6Sp0ahCJ6M&"
-        botan_url += "uid=%s&" % chat_id
-        botan_url += "name=%s" % event
-        botan = urlfetch.fetch(botan_url,
-                               payload=json.encode(message),
-                               method=urlfetch.POST,
-                               headers={"Content-Type": "application/json"})
-        if botan.status_code == 200:
-            response = json.decode(botan.content)
-            if response['status'] == 'accepted':
-                return True
-
-        return False
-    except:
-        return False
 
 
 def decode_page(page):
@@ -368,49 +161,15 @@ class DozoR(object):
                 return message
 
     def show_sessions(self, _):
-        return u"Сейчас используют:\n" + u"\n".join(CREDENTIALS.keys())
-
-    def show_sess(self, _):
         sessions = ["%s (%s)" % (v.title, k) for k, v in SESSIONS.items()]
         return u"Сейчас используют:\n" + u"\n".join(sessions)
-
-    def send_message(self, arguments):
-        try:
-            chat_id, _, text = arguments.partition(" ")
-            payload = urlencode({'chat_id': chat_id,
-                                 'text': text.encode("utf8")})
-            urlfetch.fetch(
-                "https://api.telegram.org/"
-                "bot117677541:AAEKw2NOQabXRGwr-Edvqx1RJ6Z73Xjxn8k/sendMessage",
-                payload=payload,
-                method=urlfetch.POST).content
-            return u"Сообщение отправлено"
-        except:
-            return u"Не удалось отправить сообщение"
-
-    def broadcast_message(self, text):
-        fl = True
-
-        for chat_id in SESSIONS:
-            if chat_id == self.chat_id:
-                continue
-            payload = urlencode({'chat_id': chat_id,
-                                 'text': text.encode("utf8")})
-            urlfetch.fetch(
-                "https://api.telegram.org/"
-                "bot117677541:AAEKw2NOQabXRGwr-Edvqx1RJ6Z73Xjxn8k/sendMessage",
-                payload=payload,
-                method=urlfetch.POST).content
-
-        return (u"Сообщение разослано %s получателям" % (len(SESSIONS) - 1)
-                if fl else u"Сообщение доставлено не всем")
 
     def not_found(self, _):
         return (u"Команда не найдена. Используйте /help \n"
                 u"Или дайте денег автору, и он сделает такую команду")
 
     def version(self, _):
-        return u"Версия: 3.2.2"
+        return u"Версия: 3.2.3"
 
     def help(self, _):
         return (
@@ -466,7 +225,7 @@ class DozoR(object):
     def about(self, _):
         return (u"Привет!\n"
                 u"Мой автор @m_messiah\n"
-                u"Сайт: https://m-messiah.com\n"
+                u"Мой код: https://github.com/m-messiah/dzzzzr-bot\n"
                 u"\nА еще принимаются пожертвования:\n"
                 u"https://paypal.me/muzafarov\n"
                 u"http://yasobe.ru/na/m_messiah")
@@ -595,7 +354,6 @@ class DozoR(object):
                 command = command[:command.find("@DzzzzR_bot")]
             if "@" in command:
                 return None
-            botan_track(self.chat_id, command, message)
             if command == "/set_dzzzr":
                 try:
                     return self.set_dzzzr(arguments)
@@ -640,7 +398,6 @@ class DozoR(object):
                 try:
                     result = self.gps(message['text'])
                     if result:
-                        botan_track(self.chat_id, "gps", message)
                         return result
                 except:
                     pass
@@ -665,7 +422,6 @@ class DozoR(object):
 
         if u"привет" in message['text'].lower():
             if u"бот" in message['text'].lower():
-                botan_track(self.chat_id, "hello", message)
                 return u"Привет!"
         return None
 
